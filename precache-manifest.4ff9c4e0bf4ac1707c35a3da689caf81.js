27f3b5d1534007eedfd78d3992bd2d1cc99b6c3e
Nikhil Anand <nikhil@Nikhils-MacBook-Air.local>
2019-05-27 20:26:50 +0530

    Updates

---
+++ precache-manifest.4ff9c4e0bf4ac1707c35a3da689caf81.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "638f5d31f80dd0033855fd29fe8721a1",
+    "url": "/convertico/static/media/data-size.638f5d31.svg"
+  },
+  {
+    "revision": "3ed22963259951cfa8d6",
+    "url": "/convertico/static/css/main.fdc61c4c.chunk.css"
+  },
+  {
+    "revision": "908fbe26da4bda5d15e0",
+    "url": "/convertico/static/js/runtime~main.bde72f52.js"
+  },
+  {
+    "revision": "9a2efd41632d0989cbf4",
+    "url": "/convertico/static/js/2.b4ddf05a.chunk.js"
+  },
+  {
+    "revision": "417705ce4ea961e322a62909aa3c543f",
+    "url": "/convertico/static/media/length.417705ce.svg"
+  },
+  {
+    "revision": "871a1bc03c577b2bac27cceda8fd0c0b",
+    "url": "/convertico/static/media/area.871a1bc0.svg"
+  },
+  {
+    "revision": "3ed22963259951cfa8d6",
+    "url": "/convertico/static/js/main.b71b0e46.chunk.js"
+  },
+  {
+    "revision": "8e8d9fd56e31624463ebd5a9bf3488c9",
+    "url": "/convertico/static/media/speed.8e8d9fd5.svg"
+  },
+  {
+    "revision": "7de199b177bd82023c33bef0a98ffce9",
+    "url": "/convertico/static/media/time.7de199b1.svg"
+  },
+  {
+    "revision": "c92901c3e1b5e8f80991efce1b877b3d",
+    "url": "/convertico/static/media/bg.c92901c3.png"
+  },
+  {
+    "revision": "3b012ece4568593e8d446f5b89784875",
+    "url": "/convertico/static/media/bg5.3b012ece.png"
+  },
+  {
+    "revision": "c61ebed551aa58c43ba29aed01ed251e",
+    "url": "/convertico/index.html"
+  }
+];
